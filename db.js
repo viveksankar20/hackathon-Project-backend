5a60e4a4f10cc4d2261e0691d6f443e972a04c9f
vivek7eagles <kavin@the7eagles.com>
2024-09-01 12:49:57 +0530

    text

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Your MongoDB URI
 const uri = 'mongodb+srv://viveksankar2010:vivek2001@cluster0.goqyy9w.mongodb.net/sankar-vivek';
-// Connect to MongoDB with options
+// Connect to MongoDB with optionsd
 mongoose.connect(uri)
   .then(() => {
     console.log('Connected to MongoDB successfully!');
